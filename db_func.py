f4ea47b8b53a29bb1252550dede774333008d3ec
denwayde <denwayde09@gmail.com>
2025-01-21 11:29:06 +0500

    fff

--- db_func.py
+++ db_func.py
@@ -48,50 +48,68 @@ def insert_many(insertion_query, lst):
     connection.commit()
 
 
-# last_checkpoint = select_data("SELECT*FROM user_points WHERE point_name = ? AND telega_id = ? AND date = ? ORDER BY id DESC LIMIT 1", ('qq', 1949640271, '2024-12-22'))[0]
+#from datetime import datetime, timedelta
 
-# print(last_checkpoint[3])
-# sqldata = select_data_dict("SELECT*FROM userpoints_weekly INNER JOIN usernames ON userpoints_weekly.telega_id = usernames.telega_id WHERE date_date BETWEEN '2023-10-01' AND '2023-10-31'")
-# #print(sqldata)
-# res = [dict(row) for row in sqldata]
-# print(res)
-
-# print()
-# print()
-
-dic = [
-    {'id': 1, 'point_name': 'qq', 'telega_id': 6293086969, 'point_score': 12, 'date': '2024-12-26', 'name': 'dd', 'hour': None, 'minute': None, 'period': None},
-    {'id': 2, 'point_name': 'djf', 'telega_id': 6293086969, 'point_score': 10, 'date': '2024-12-26', 'name': 'dd', 'hour': None, 'minute': None, 'period': None},
-    {'id': 3, 'point_name': 'ввв', 'telega_id': 6293086969, 'point_score': 23, 'date': '2024-12-26', 'name': 'dd', 'hour': None, 'minute': None, 'period': None}, {'id': 4, 'point_name': 'ффф', 'telega_id': 6293086969, 'point_score': 23, 'date': '2024-12-26', 'name': 'dd', 'hour': None, 'minute': None, 'period': None}, {'id': 5, 'point_name': 'qq', 'telega_id': 1949640271, 'point_score': 11, 'date': '2024-12-26', 'name': 'Admin', 'hour': None, 'minute': None, 'period': None}, {'id': 6, 'point_name': 'djf', 'telega_id': 1949640271, 'point_score': 34, 'date': '2024-12-26', 'name': 'Admin', 'hour': None, 'minute': None, 'period': None}, {'id': 7, 'point_name': 'ввв', 'telega_id': 1949640271, 'point_score': 45, 'date': '2024-12-26', 'name': 'Admin', 'hour': None, 'minute': None, 'period': None}, {'id': 8, 'point_name': 'ффф', 'telega_id': 1949640271, 'point_score': 56, 'date': '2024-12-26', 'name': 'Admin', 'hour': None, 'minute': None, 'period': None}, {'id': 9, 'point_name': 'qq', 'telega_id': 1949640271, 'point_score': 12, 'date': '2024-12-26', 'name': 'Admin', 'hour': None, 'minute': None, 'period': None}, {'id': 10, 'point_name': 'qq', 'telega_id': 6293086969, 'point_score': 33, 'date': '2024-12-26', 'name': 'dd', 'hour': None, 'minute': None, 'period': None}, {'id': 11, 'point_name': 'djf', 'telega_id': 6293086969, 'point_score': 11, 'date': '2024-12-26', 'name': 'dd', 'hour': None, 'minute': None, 'period': None}
-    ]
-
-points = select_data_single("SELECT name FROM points")#[('kkk',), ('джф',), ('kit',), ('hggg',)]
-usernames = select_data_single("SELECT name FROM usernames ORDER BY name")
-result = []
-
-
+# today = datetime.now()
+# # Находим номер сегодняшнего дня недели (0 - понедельник, 6 - воскресенье)
+# today_weekday = today.weekday()
+# # Вычисляем дату понедельника текущей недели
+# monday = today - timedelta(days=today_weekday)
+# # Вычисляем дату воскресенья текущей недели
+# sunday = monday + timedelta(days=6)
+# sqldata = select_data_dict("SELECT * FROM userpoints_weekly INNER JOIN usernames ON userpoints_weekly.telega_id = usernames.telega_id INNER JOIN points ON points.name = userpoints_weekly.point_name WHERE date BETWEEN ? AND ?", (monday.date(), sunday.date(), ))
+# dic = [dict(row) for row in sqldata]
+# print(dic)
 
 from datetime import datetime, timedelta
 
-# Получаем сегодняшнюю дату
+# Получаем текущую дату
 today = datetime.now()
 
-# Находим номер сегодняшнего дня недели (0 - понедельник, 6 - воскресенье)
-today_weekday = today.weekday()
-
-# Вычисляем дату понедельника текущей недели
-monday = today - timedelta(days=today_weekday)
-
-# Вычисляем дату воскресенья текущей недели
+# Находим первый день недели (понедельник)
+monday = today - timedelta(days=today.weekday())
 sunday = monday + timedelta(days=6)
+# Создаем массив дат этой недели
+week_dates = [monday + timedelta(days=i) for i in range(7)]
 
-# sqldata = select_data_dict(f"SELECT*FROM userpoints_weekly INNER JOIN usernames ON userpoints_weekly.telega_id = usernames.telega_id WHERE date_date BETWEEN {monday.date()} AND {sunday.date()}")
+# Преобразуем даты в строки для удобного отображения (опционально)
 
+sqldata = select_data_dict("SELECT * FROM userpoints_weekly INNER JOIN usernames ON userpoints_weekly.telega_id = usernames.telega_id INNER JOIN points ON points.name = userpoints_weekly.point_name WHERE date BETWEEN ? AND ?", (monday.date(), sunday.date(), ))
+dic = [dict(row) for row in sqldata]
 
-# Форматируем вывод в нужном виде
-# print("Понедельник текущей недели:", monday.date())
-# print("Воскресенье текущей недели:", sunday.date())
+# print(dic)
+week_dates_str = [date.strftime('%Y-%m-%d') for date in week_dates]
+points = select_data_single("SELECT name FROM points")
+usernames = select_data_single("SELECT name FROM usernames ORDER BY name")
+pred_result = []
+
+data = [
+    {'id': 12, 'point_name': 'qq', 'telega_id': 6293086969, 'point_score': 11, 'date': '2025-01-20', 'name': 'dd', 'hour': None, 'minute': None, 'period': None, 'ratio': 1, 'mins': 10},
+    {'id': 16, 'point_name': 'qq', 'telega_id': 6293086969, 'point_score': 12, 'date': '2025-01-20', 'name': 'dd', 'hour': None, 'minute': None, 'period': None, 'ratio': 1, 'mins': 10}, {'id': 13, 'point_name': 'djf', 'telega_id': 6293086969, 'point_score': 21, 'date': '2025-01-20', 'name': 'dd', 'hour': None, 'minute': None, 'period': None, 'ratio': 2, 'mins': 30}, {'id': 14, 'point_name': 'qq', 'telega_id': 1949640271, 'point_score': 10, 'date': '2025-01-20', 'name': 'Admin', 'hour': None, 'minute': None, 'period': None, 'ratio': 1, 'mins': 10}, {'id': 15, 'point_name': 'djf', 'telega_id': 1949640271, 'point_score': 20, 'date': '2025-01-20', 'name': 'Admin', 'hour': None, 'minute': None, 'period': None, 'ratio': 2, 'mins': 30}
+    ]
+
+for name in usernames:
+    # print(name)
+    dd = {'name': name}
     
+    for week_day in week_dates_str:
+        dp = {}
+        for point in points:
+            dp[point] = 0
+        for data_element in data:
+            # print(f"{week_day} == {data_element['date']} and {name} == {data_element['name']}")
+            # print(week_day == data_element['date'])
+            if name == data_element['name'] and week_day == data_element['date']:
+                dp[data_element['point_name']] += data_element['point_score']
+            #     print(dp)
+            # print()
+        dd[week_day] = dp 
+        # print(dd)   
+    pred_result.append(dd)
+
+print(pred_result)
+
+
 
 
 
--- excel_creator.py
+++ excel_creator.py
@@ -1,25 +1,21 @@
 from openpyxl import Workbook
-from db_func import select_data
-import datetime
+from db_func import select_data, select_data_dict, select_data_single
+# import datetime
+from datetime import datetime, timedelta
 
-def exsel_creator():
-    wb = Workbook()
-    ws = wb.active
-
-    points = select_data("SELECT name FROM points")#[('kkk',), ('джф',), ('kit',), ('hggg',)]
-    usernames = select_data("SELECT name FROM usernames ORDER BY name")
+def result_total():
+    # wb = Workbook()
+    # ws = wb.active
+    just_points = select_data_single("SELECT name FROM points")#[('kkk',), ('джф',), ('kit',), ('hggg',)]
+    just_usenames = select_data_single("SELECT name FROM usernames ORDER BY name")
     users_points = select_data("SELECT* FROM user_points INNER JOIN points ON points.name = user_points.point_name INNER JOIN usernames ON user_points.telega_id = usernames.telega_id ORDER BY usernames.name")
-
-    day = datetime.datetime.now().strftime("%Y-%m-%d")
+    #day = datetime.datetime.now().strftime("%Y-%m-%d")
     # print(users_points)
-
-    just_usenames = [x[0] for x in usernames]
-    just_points = [x[0] for x in points]
+    # just_usenames = [x[0] for x in usernames]
+    # just_points = [x[0] for x in points]
+    # points_release = ["", *just_points, "Сумма", "Сумма c учетом коэффициентов"]
+    # ws.append(points_release)
     users = []
-
-    points_release = ["", *just_points, "Сумма", "Сумма c учетом коэффициентов"]
-    ws.append(points_release)
-
     for x in just_usenames:
         my_dict = {}
         my_dict["name"] = x
@@ -36,68 +32,55 @@ def exsel_creator():
                 x[v[1]] = v[3]
                 x['sum'] = x['sum'] + v[3]
                 x['ksum'] = x['ksum'] + (v[3]*v[7])
-
     #[{'name': 'Admin', 'kkk': 12, 'джф': 20, 'kit': 25, 'hggg': 1}, {'name': 'динамо', 'kkk': '', 'джф': '', 'kit': '', 'hggg': ''}, {'name': 'цска', 'kkk': 8, 'джф': 20, 'kit': 40, 'hggg': 2}]
-
     result = []
     for x in users:
         result.append([*x.values()])
-
+    return result
     #print(result)
-    for x in result:
-        ws.append(x) 
+    # for x in result:
+    #     ws.append(x) 
 
-    wb.save(f"excels/{day}.xlsx")      
-
-from functions import is_date_in_current_week
-
-sqldata = select_data("SELECT*FROM userpoints_weekly INNER JOIN usernames ON userpoints_weekly.telega_id = usernames.telega_id")
-#print(sqldata)
-
-result_data = []
-
-points = select_data("SELECT name FROM points")#[('kkk',), ('джф',), ('kit',), ('hggg',)]
-usernames = select_data("SELECT name FROM usernames ORDER BY name")
-just_usenames = [x[0] for x in usernames]
-just_points = [x[0] for x in points]
-users = []
-
-for x in just_usenames:
-        my_dict = {}
-        my_dict["name"] = x
-        for z in just_points:
-            my_dict[z] = ""
-        my_dict["sum"] = 0
-        my_dict["ksum"] = 0
-        users.append(
-            my_dict
-        )
-
-#print(users)#[{'name': 'Admin', 'qq': '', 'djf': '', 'ввв': '', 'ффф': '', 'sum': 0, 'ksum': 0}, {'name': 'dd', 'qq': '', 'djf': '', 'ввв': '', 'ффф': '', 'sum': 0, 'ksum': 0}]
-
-for x in users:
-    for z in sqldata:
-        if is_date_in_current_week(z[4]):
-            if x['name'] in z:
-
-                
-
-# def exel_weekly():
-#     data = select_data("SELECT*FROM userpoints_weekly")#[]
-#     print(data)
+    # wb.save(f"excels/{day}.xlsx")      
 
 
-# exel_weekly()
-[
-    (1, 'qq', 6293086969, 12, '2024-12-26', 16, 'dd', 6293086969, None, None, None), 
-    (2, 'djf', 6293086969, 10, '2024-12-26', 16, 'dd', 6293086969, None, None, None), 
-    (3, 'ввв', 6293086969, 23, '2024-12-26', 16, 'dd', 6293086969, None, None, None), 
-    (4, 'ффф', 6293086969, 23, '2024-12-26', 16, 'dd', 6293086969, None, None, None), 
-    (5, 'qq', 1949640271, 11, '2024-12-26', 15, 'Admin', 1949640271, None, None, None), 
-    (6, 'djf', 1949640271, 34, '2024-12-26', 15, 'Admin', 1949640271, None, None, None), 
-    (7, 'ввв', 1949640271, 45, '2024-12-26', 15, 'Admin', 1949640271, None, None, None), 
-    (8, 'ффф', 1949640271, 56, '2024-12-26', 15, 'Admin', 1949640271, None, None, None),
-    (9, 'qq', 1949640271, 12, '2024-12-26', 15, 'Admin', 1949640271, None, None, None),
-    (10, 'qq', 6293086969, 33, '2024-12-26', 16, 'dd', 6293086969, None, None, None),
-    (11, 'djf', 6293086969, 11, '2024-12-26', 16, 'dd', 6293086969, None, None, None)
-]
+
+def result_weekly():
+    # Получаем сегодняшнюю дату
+    today = datetime.now()
+    # Находим номер сегодняшнего дня недели (0 - понедельник, 6 - воскресенье)
+    today_weekday = today.weekday()
+    # Вычисляем дату понедельника текущей недели
+    monday = today - timedelta(days=today_weekday)
+    # Вычисляем дату воскресенья текущей недели
+    sunday = monday + timedelta(days=6)
+    sqldata = select_data_dict("SELECT * FROM userpoints_weekly INNER JOIN usernames ON userpoints_weekly.telega_id = usernames.telega_id INNER JOIN points ON points.name = userpoints_weekly.point_name WHERE date BETWEEN ? AND ?", (monday.date(), sunday.date(), ))  #--------------------VOT ETO TEBE OCHEN NUJNO
+    #print([dict(row) for row in sqldata])
+    dic = [dict(row) for row in sqldata]
+    points = select_data_single("SELECT name FROM points")#[('kkk',), ('джф',), ('kit',), ('hggg',)]
+    usernames = select_data_single("SELECT name FROM usernames ORDER BY name")
+    pred_result = []
+
+    for x in usernames:
+        dd = {"name": x}
+        for z in points:
+            dd[z] = 0
+        dd["sum"] = 0
+        dd["ksum"] = 0
+        pred_result.append(dd)
+    # print(pred_result) #------------[ {'name': 'Admin', 'qq': 0, 'djf': 0, 'ввв': 0, 'ффф': 0}, {'name': 'dd', 'qq': 0, 'djf': 0, 'ввв': 0, 'ффф': 0} ]
+    for c in dic:
+        for z in pred_result:
+            if z['name'] == c['name']:
+                z[c['point_name']]+=c['point_score']#-------------------ZAPOLNYAEM RESULT CHISLAMI
+                z['sum']+=c['point_score']
+                z['ksum']+=c['point_score']*c['ratio']
+    #print(pred_result) #-------[{'name': 'Admin', 'qq': 23, 'djf': 34, 'ввв': 45, 'ффф': 56}, {'name': 'dd', 'qq': 45, 'djf': 21, 'ввв': 23, 'ффф': 23}]
+
+    result = []
+    for x in pred_result:
+        result.append([*x.values()])    #tut vmesto novogo cikla luche srazu sdelat ws.append([*x.values()])
+    return result
+
+# print(result_weekly())
+#print(result_total())
